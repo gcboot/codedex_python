2b8dd12c901225a86d9d8fe855362e1c2fbd4387
VictorCun <vmchero@gmail.com>
2023-02-01 21:33:32 -0600

    Ejercico pattern

---
+++ 02_pattern.py
@@ -0,0 +1,10 @@
+""" Suppose we want the output to look exactly like this pattern below:
+       1
+     2 3
+   4 5 6
+7 8 9 10
+"""
+print('       1')
+print('     2 3')
+print('   4 5 6')
+print('7 8 9 10')
