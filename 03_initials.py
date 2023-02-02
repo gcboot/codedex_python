3f31f2324bc4776e8e78716f5afd4263f7fb9cec
VictorCun <vmchero@gmail.com>
2023-02-01 21:43:47 -0600

    Ejerccio initials

---
+++ 03_initials.py
@@ -0,0 +1,15 @@
+""" A    L
+ A A   L
+A   A  L
+AAAAA  L
+A   A  L
+A   A  L
+A   A  LLLLL
+"""
+
+print('V          V     C C C C')
+print(' V        V    C        ')
+print('  V      V     C        ')
+print('   V    V      C        ')
+print('    V  V       C        ')
+print('     V           C C C C')
