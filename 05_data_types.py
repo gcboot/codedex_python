90d31d55566913c1e88738ab65b570ba0bb17806
VictorCun <vmchero@gmail.com>
2023-02-01 22:02:49 -0600

    Data Types

---
+++ 05_data_types.py
@@ -0,0 +1,26 @@
+# Variables
+
+name = "Victor"
+user_id = 20230201
+progress = 0.75
+exp = 60
+verified = True
+
+# Data Types
+
+# Int
+year = 2023
+age = 32
+
+# Float
+pi = 3.14159
+meal_cost = 12.99
+
+# String
+message = "Goode morning"
+user = "@gcboot"
+
+# Boolean
+late_to_class = False
+cranky = True
+
